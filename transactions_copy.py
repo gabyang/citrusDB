a095380ef930a6b8bb44ee6e0c0b985c508a395c
gabyang <e0725795@u.nus.edu>
2024-09-13 18:02:44 +0800

    wrap new order in transaciton

--- transactions_copy.py
+++ transactions_copy.py
@@ -88,102 +88,107 @@ class Transactions:
             supplier_warehouses (List[int]): List of supplier warehouse IDs
             quantities (List[int]): List of quantities for each item
         """
+        try:
+            self.cursor.execute("BEGIN")
 
-        orderline_inputs = []
-        for i in range(num_items):
-            orderline_inputs.append((
-                item_ids[i],
-                supplier_warehouses[i],
-                quantities[i]
-            ))
+            orderline_inputs = []
+            for i in range(num_items):
+                orderline_inputs.append((
+                    item_ids[i],
+                    supplier_warehouses[i],
+                    quantities[i]
+                ))
 
-        # Check if all items are from the same warehouse
-        is_all_local = int(all(warehouse_id == w_id for warehouse_id in supplier_warehouses))
+            # Check if all items are from the same warehouse
+            is_all_local = int(all(warehouse_id == w_id for warehouse_id in supplier_warehouses))
 
-        # step 1 and step 2
-        next_order_id = None
-        self.cursor.execute("SELECT D_NEXT_O_ID FROM district WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
-        next_order_id = self.cursor.fetchone()[0] + 1
-        self.cursor.execute("UPDATE district SET D_NEXT_O_ID = D_NEXT_O_ID + 1 WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
+            # step 1 and step 2
+            next_order_id = None
+            self.cursor.execute("SELECT D_NEXT_O_ID FROM district WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
+            next_order_id = self.cursor.fetchone()[0] + 1
+            self.cursor.execute("UPDATE district SET D_NEXT_O_ID = D_NEXT_O_ID + 1 WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
 
-        # step 3
-        entry_time = datetime.now(timezone.utc)
-        self.cursor.execute("INSERT INTO \"order\"(O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL) VALUES (%s, %s, %s, %s, %s, NULL, %s, %s)", 
-                            (next_order_id, d_id, w_id, c_id, entry_time, num_items, is_all_local))
-        
-        # Update stock and calculate total amount
-        total_amount = 0
-        orderline_outputs = []
-        stock_deltas = []
+            # step 3
+            entry_time = datetime.now(timezone.utc)
+            self.cursor.execute("INSERT INTO \"order\"(O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL) VALUES (%s, %s, %s, %s, %s, NULL, %s, %s)", 
+                                (next_order_id, d_id, w_id, c_id, entry_time, num_items, is_all_local))
+            
+            # Update stock and calculate total amount
+            total_amount = 0
+            orderline_outputs = []
+            stock_deltas = []
 
-        # retrieve item prices
-        self.cursor.execute("SELECT I_PRICE, I_NAME FROM item WHERE I_ID = ANY(%s)", (item_ids,))
-        result = self.cursor.fetchall()
-        item_prices = [round(float(x[0]), 2) for x in result]
-        item_names = [x[1] for x in result]
+            # retrieve item prices
+            self.cursor.execute("SELECT I_PRICE, I_NAME FROM item WHERE I_ID = ANY(%s)", (item_ids,))
+            result = self.cursor.fetchall()
+            item_prices = [round(float(x[0]), 2) for x in result]
+            item_names = [x[1] for x in result]
 
-        # retrieve distance information
-        stock_dist_id = f'S_DIST_0{d_id}' if d_id < 10 else f'S_DIST_{d_id}'
-        query = f"SELECT {stock_dist_id} FROM stock WHERE S_I_ID = ANY(%s) AND S_W_ID = {w_id}"
-        self.cursor.execute(query, (item_ids,))
-        result = self.cursor.fetchall()
-        ol_dist = [x[0] for x in result]
-        # TODO: check if the ol_dist needs to be stripped of its blank spaces - note that the distances already have blank spaces.
+            # retrieve distance information
+            stock_dist_id = f'S_DIST_0{d_id}' if d_id < 10 else f'S_DIST_{d_id}'
+            query = f"SELECT {stock_dist_id} FROM stock WHERE S_I_ID = ANY(%s) AND S_W_ID = {w_id}"
+            self.cursor.execute(query, (item_ids,))
+            result = self.cursor.fetchall()
+            ol_dist = [x[0] for x in result]
+            # TODO: check if the ol_dist needs to be stripped of its blank spaces - note that the distances already have blank spaces.
 
-        # TODO: Optimize the for loop below to update ALL if that is possible.
-        for idx, ol in enumerate(orderline_inputs):
-            ol_item_id = ol[0]
-            ol_warehouse = ol[1]
-            ol_quantity = ol[2]
-            self.cursor.execute("SELECT S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT FROM stock WHERE S_I_ID = %s AND S_W_ID = %s", 
-                (ol_item_id, ol_warehouse))
+            # TODO: Optimize the for loop below to update ALL if that is possible.
+            for idx, ol in enumerate(orderline_inputs):
+                ol_item_id = ol[0]
+                ol_warehouse = ol[1]
+                ol_quantity = ol[2]
+                self.cursor.execute("SELECT S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT FROM stock WHERE S_I_ID = %s AND S_W_ID = %s", 
+                    (ol_item_id, ol_warehouse))
 
-            quantity, ytd, order_count, remote_count = self.cursor.fetchone()
+                quantity, ytd, order_count, remote_count = self.cursor.fetchone()
 
-            next_quantity = quantity + ol_quantity
-            next_quantity = next_quantity + 100 if next_quantity < 10 else next_quantity
-            next_ytd = ytd + ol_quantity
-            next_order_count = order_count + 1
-            next_remote_count = remote_count + (1 if ol_warehouse != w_id else 0)
+                next_quantity = quantity + ol_quantity
+                next_quantity = next_quantity + 100 if next_quantity < 10 else next_quantity
+                next_ytd = ytd + ol_quantity
+                next_order_count = order_count + 1
+                next_remote_count = remote_count + (1 if ol_warehouse != w_id else 0)
 
-            self.cursor.execute("UPDATE stock SET S_QUANTITY = %s, S_YTD = %s, S_ORDER_CNT = %s, S_REMOTE_CNT = %s WHERE s_w_id = %s AND s_i_id = %s", 
-                (next_quantity, next_ytd, next_order_count, next_remote_count, ol_warehouse, ol_item_id)
-            )
+                self.cursor.execute("UPDATE stock SET S_QUANTITY = %s, S_YTD = %s, S_ORDER_CNT = %s, S_REMOTE_CNT = %s WHERE s_w_id = %s AND s_i_id = %s", 
+                    (next_quantity, next_ytd, next_order_count, next_remote_count, ol_warehouse, ol_item_id)
+                )
 
-            item_amount = item_prices[idx] * ol_quantity
-            total_amount += item_amount
+                item_amount = item_prices[idx] * ol_quantity
+                total_amount += item_amount
 
-            orderline_outputs.append([
-                ol_item_id, item_names[idx], ol_warehouse, ol_quantity, item_amount, next_quantity
-            ])
+                orderline_outputs.append([
+                    ol_item_id, item_names[idx], ol_warehouse, ol_quantity, item_amount, next_quantity
+                ])
 
-            # step 5
-            self.cursor.execute(
-                "INSERT INTO \"order-line\"(ol_w_id, ol_d_id, ol_o_id, ol_number, ol_i_id, ol_amount, ol_supply_w_id, ol_quantity, ol_dist_info, ol_delivery_d) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)",
-                (w_id, d_id, next_order_id, idx + 1, ol_item_id, item_prices[idx], ol_warehouse, ol_quantity, ol_dist[idx])
-            )
-        
-        self.cursor.execute("SELECT d_tax FROM district WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
-        d_tax = round(float(self.cursor.fetchone()[0]), 2)
-        print(d_tax)
+                # step 5
+                self.cursor.execute(
+                    "INSERT INTO \"order-line\"(ol_w_id, ol_d_id, ol_o_id, ol_number, ol_i_id, ol_amount, ol_supply_w_id, ol_quantity, ol_dist_info, ol_delivery_d) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)",
+                    (w_id, d_id, next_order_id, idx + 1, ol_item_id, item_prices[idx], ol_warehouse, ol_quantity, ol_dist[idx])
+                )
+            
+            self.cursor.execute("SELECT d_tax FROM district WHERE D_ID = %s AND D_W_ID = %s", (d_id, w_id))
+            d_tax = round(float(self.cursor.fetchone()[0]), 2)
+            print(d_tax)
 
-        self.cursor.execute("SELECT w_tax FROM warehouse WHERE W_ID = %s", (w_id,))
-        w_tax = round(float(self.cursor.fetchone()[0]), 4)
-        print(w_tax)
+            self.cursor.execute("SELECT w_tax FROM warehouse WHERE W_ID = %s", (w_id,))
+            w_tax = round(float(self.cursor.fetchone()[0]), 4)
+            print(w_tax)
 
-        self.cursor.execute("SELECT c_discount, c_last, c_credit FROM customer WHERE c_w_id = %s AND c_d_id = %s AND c_id = %s", (w_id, d_id, c_id))
-        c_discount, c_last, c_credit = self.cursor.fetchone()
-        print(c_discount, c_last, c_credit)
+            self.cursor.execute("SELECT c_discount, c_last, c_credit FROM customer WHERE c_w_id = %s AND c_d_id = %s AND c_id = %s", (w_id, d_id, c_id))
+            c_discount, c_last, c_credit = self.cursor.fetchone()
+            print(c_discount, c_last, c_credit)
 
-        print(next_order_id, entry_time)
+            print(next_order_id, entry_time)
 
-        # step 6 
-        total_amount = total_amount * (1 + d_tax + w_tax) * (1 - c_discount)
-        print(num_items, total_amount)
+            # step 6 
+            total_amount = total_amount * (1 + d_tax + w_tax) * (1 - float(c_discount))
+            print(num_items, total_amount)
 
-        print(orderline_outputs)
+            print(orderline_outputs)
+            # self.cursor.execute("COMMIT")
 
-
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(f"An error occurred: {error}")
+            self.cursor.execute("ROLLBACK")
         return
 
     # 2.2 payment transaction
