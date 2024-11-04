a254fde725a06e35d3d4890f0a77176b5e1ec43f
Merrick <merrickneo@gmail.com>
2024-11-04 17:21:56 +0800

    Fix indexing

--- setup/bash-scripts/test-run/metrics/throughput.py
+++ setup/bash-scripts/test-run/metrics/throughput.py
@@ -5,10 +5,10 @@ def throughput():
         # Open the clients.csv file and read in the 3rd column into a list
         # Get the minimum, average and maximum values of the list
         # Write the values to the throughput.csv file
-        with open("performance_metrics.csv", "r") as c:
+        with open("clients.csv", "r") as c:
             reader = csv.reader(c)
             next(reader)
-            values = [float(row[2]) for row in reader]
+            values = [float(row[3]) for row in reader]
         min_val = round(min(values), 2)
         avg_val = round(sum(values) / len(values), 2)
         max_val = round(max(values), 2)
